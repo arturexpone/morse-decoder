f87188bb2f36bad9b8aca3dda2312d609712aa9d
arturexpone <arturexpone@yandex.ru>
2020-10-13 10:30:28 +0300

    feat: resolve morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const DOT = '.';
+    const DASH = '-';
+    let result = '';
+
+    const arrExpr = [...expr];
+    let resultArr = [];
+    while(arrExpr.length > 0) {
+        let arr = arrExpr.splice(0, 10);
+        resultArr.push(arr);
+    }
+    resultArr = resultArr.map((item, index) => {
+        for(let i = 0; i < item.length; i++) {
+            if(item[i] === '*') {
+                return item.join('');
+            }else if(item[i] === '1') {
+                item = item.slice(i);
+                return item;
+            }
+        }
+    }).map((item) => {
+        for (let i = 0; i < item.length; i++) {
+            if(Array.isArray(item)) {
+                item[i] = item.splice(i, 2, (item[i] + item[i + 1])).join('');
+            }
+        }
+        return item;
+    }).map(item => {
+        if(Array.isArray(item)) {
+            item.map((symbol, index) => {
+                if(symbol === '10') {
+                    return item[index] = DOT;
+                }else if(symbol === '11') {
+                    return item[index] = DASH;
+                }
+            })
+            return item.join('');
+        }
+    }).forEach(item => {
+        if(item === undefined) {
+            result += ' ';
+        }else {
+            result += MORSE_TABLE[item];
+        }
+    })
+    return result;
 }
 
 module.exports = {
